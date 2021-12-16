7f51cb0db64a6507bec825783f1619f4e55fceeb
l33th <l33th@protonmail.ch>
2021-12-16 08:58:42 +0200

    create_new_password function added

--- p455w0rd5.py
+++ p455w0rd5.py
@@ -3,7 +3,8 @@ import sqlite3
 
 def create_table():
     db = sqlite3.connect('.database.db')
-    statement = '''CREATE TABLE if not exists MANAGER(
+    statement = '''
+    CREATE TABLE if not exists MANAGER(
         ID INTEGER PRIMARY KEY AUTOINCREMENT,
         WEBSITE TEXT NOT NULL,
         PASSWORD TEXT NOT NULL,
@@ -18,3 +19,23 @@ def create_table():
 
 
 create_table()
+
+
+def create_new_password():
+    db = sqlite3.connect('.database.db')
+    statement = '''
+    INSERT INTO MANAGER(
+        WEBSITE, PASSWORD, EMAIL
+    )
+    VALUES (?,?,?)
+    '''
+
+    website = input('Enter the name of the website: ')
+    password = input(f'Enter the password for {website}: ')
+    email = input(f'Enter th Email associated with {website}: ')
+    cur = db.cursor()
+    cur.execute(statement, (website, password, email))
+    db.commit()
+    db.close()
+    print(f'password for {website} has been successfully created.')
+
